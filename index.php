ab318c9f8fae2fbb796687f9a1195784f878ae24
Jordan Murphy <98192871+jmurphy007@users.noreply.github.com>
2023-01-13 18:26:02 -0500

    Update

--- index.php
+++ index.php
@@ -6,34 +6,8 @@
 		<meta name="author" content="Jordan Murphy" />
 		<meta name="description" content="Jordan Murphy's Portfolio for Game Development and Photography"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		
-		<style>
-			.topnav{
-				background-color: #333;
-				overflow: hidden;
-			}
-			.topnav a {
-				float: left;
-				color: #f2f2f2;
-				text-align: center;
-				padding: 14px 16px;
-				text-decoration: none;
-				font-size: 17px;
-			}
-			.topnav a:hover {
-				background-color: #ddd;
-				color: black;
-			}
-		</style>
 	</head>
 	<body>
-		<div class = "topnav">
-			<a href="#home">Home</a>
-			<a href="#game">Game Dev</a>
-			<a href="#photo">Photography</a>
-			<a href="#about">About Me</a>
-		</div>	
-			
 		<h1>Hello World</h1>
 		<p>I'm hosted with GitHub Pages.</p>
 	</body>
