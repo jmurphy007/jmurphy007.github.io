00418e0459c5821dd88d2c4cb9c8224bd7c397b6
Jordan Murphy <98192871+jmurphy007@users.noreply.github.com>
2023-01-13 18:04:22 -0500

    Update header.php

--- view/header.php
+++ view/header.php
@@ -4,6 +4,33 @@
 		<title>Jordan Murphy Portfolio Page</title>
 		<meta charset="utf-8" />
 		<meta name="author" content="Jordan Murphy" />
-		<meta name="description" con/>
+		<meta name="description" content="Jordan Murphy's Portfolio for Game Development and Photography"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<style>
+			.topnav{
+				background-color: #333;
+				overflow: hidden;
+			}
+			.topnav a {
+				float: left;
+				color: #f2f2f2;
+				text-align: center;
+				padding: 14px 16px;
+				text-decoration: none;
+				font-size: 17px;
+			}
+			.topnav a:hover {
+				background-color: #ddd;
+				color: black;
+			}
+		</style>
 	</head>
+	<body>
+		<div class = "topnav">
+			<a href="#home">Home</a>
+			<a href="#game">Game Dev</a>
+			<a href="#photo">Photography</a>
+			<a href="#about">About Me</a>
+		</div>	
+			
