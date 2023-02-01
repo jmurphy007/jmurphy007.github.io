efb323b82d47858c43eab260de93097e5bd936e0
Jordan Murphy <98192871+jmurphy007@users.noreply.github.com>
2023-01-31 20:10:12 -0500

    Call for nav bars instead of multiple implementations

--- Components/topButton.js
+++ Components/topButton.js
@@ -1,3 +1,11 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+	<button onclick="toTop()" id="topBtn" title="Go to top">&#8593;</button>
+`;
+
+document.body.appendChild(template.content);
+
 let mybutton = document.getElementById("topBtn");
 window.onscroll = function() {scrollFunction()};
 
