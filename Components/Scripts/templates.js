e3ed45334ad7b81f46e3b747bef75efbd03c7026
Jordan Murphy <98192871+jmurphy007@users.noreply.github.com>
2023-01-31 22:05:16 -0500

    Update templates.js

--- Components/Scripts/templates.js
+++ Components/Scripts/templates.js
@@ -2,16 +2,7 @@ const template = document.createElement('template');
 
 const currentFile = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
 
-if (currentFile == "index.html"){
-	template.innerHTML = `
-		<div class = "topnav">
-			<a class = "active" href="index.html">Home</a>
-			<a href="Pages/gameDev.html">Game Dev</a>
-			<a href="Pages/photo.html">Photography</a>
-			<a href="Pages/about.html">About</a>
-		</div>
-	`;
-} else if (currentFile == "about.html"){
+if (currentFile == "about.html"){
 	template.innerHTML = `
 		<div class = "topnav">
 			<a href="../index.html">Home</a>
@@ -36,6 +27,15 @@ if (currentFile == "index.html"){
 			<a href="gameDev.html">Game Dev</a>
 			<a class = "active" href="photo.html">Photography</a>
 			<a href="about.html">About</a>
+		</div>
+	`;
+} else {
+	template.innerHTML = `
+		<div class = "topnav">
+			<a class = "active" href="index.html">Home</a>
+			<a href="Pages/gameDev.html">Game Dev</a>
+			<a href="Pages/photo.html">Photography</a>
+			<a href="Pages/about.html">About</a>
 		</div>
 	`;
 }
